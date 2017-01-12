ec9c12d85180dfebe00a76996f549af138614a55
Diego Pedroza <Diego Pedroza>
2017-01-12 10:49:25 -0500

    database fix

--- src/common/database.py
+++ src/common/database.py
@@ -11,7 +11,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client.get_database("heroku_m2qpb93s")
 
     @staticmethod
     def insert(collection, data):
